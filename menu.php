d2146906f22f3a59ae3cf0ace2f1e674615ad392
Dida Haiman Irtsa <93466886+N2Deepp@users.noreply.github.com>
2023-07-27 17:31:34 +0700

    Menambahkan File, CSS,IMG,JS dan Menu

---
+++ menu.php
@@ -0,0 +1,79 @@
+<?php
+require_once('header.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Kopi Kenangan</title>
+
+    <!-- My Icon -->
+    <link rel="shortcut icon" href="img/menu/1.jpg" />
+
+    <!-- My Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap" rel="stylesheet" />
+
+    <!-- My Feather Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- My Style -->
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <!-- Menu Section Start -->
+    <section id="menu" class="menu">
+        <h2><span>Menu</span> Kami</h2>
+        <p>
+            kopi yang kami jual adalah murni dari hasil yang kami panen sendri
+        </p>
+        <div class="row">
+            <div class="menu-card">
+                <img src="img/menu/1.jpg" alt="Espreso" class="menu-card-img" />
+                <h3 class="menu-card-title">- Espresso -</h3>
+                <p class="menu-card-price">IDR 15K</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/1.jpg" alt="Espreso" class="menu-card-img" />
+                <h3 class="menu-card-title">- Espresso -</h3>
+                <p class="menu-card-price">IDR 15K</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/1.jpg" alt="Espreso" class="menu-card-img" />
+                <h3 class="menu-card-title">- Espresso -</h3>
+                <p class="menu-card-price">IDR 15K</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/1.jpg" alt="Espreso" class="menu-card-img" />
+                <h3 class="menu-card-title">- Espresso -</h3>
+                <p class="menu-card-price">IDR 15K</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/1.jpg" alt="Espreso" class="menu-card-img" />
+                <h3 class="menu-card-title">- Espresso -</h3>
+                <p class="menu-card-price">IDR 15K</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/1.jpg" alt="Espreso" class="menu-card-img" />
+                <h3 class="menu-card-title">- Espresso -</h3>
+                <p class="menu-card-price">IDR 15K</p>
+            </div>
+        </div>
+    </section>
+    <!-- Menu Section End -->
+
+
+    <!-- Footer Start -->
+    <?php
+    require_once('footer.php');
+    ?>
+
+    <!-- Footer End -->
+</body>
+
+</html>
